7df81f73f7e14b0913653edd150be90616397373
AlioshynaSasha <a.aleshina2000@gmail.com>
2022-10-29 20:08:14 +0300

    Allure_report

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -2,18 +2,23 @@ package baseEntities;
 
 import configuration.ReadProperties;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import services.BrowsersService;
 import steps.LoginStep;
+import utils.InvokedListener;
 
+@Listeners(InvokedListener.class)
 public abstract class BaseTest {
     protected WebDriver driver;
     protected LoginStep loginStep;
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp(ITestContext iTestContext) {
         driver = new BrowsersService().getDriver();
+        iTestContext.setAttribute("driver", driver);
         driver.get(ReadProperties.getUrl());
 
         loginStep = new LoginStep(driver);
